3a633f6cbca92d8a82cc11d87083bbb5833abc02
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-04-25 00:18:49 +0200

    inserito script axios+ myFunction +

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,20 @@ const { createApp } = Vue
 
 createApp({
   data(){
-
+    
   },
   mounted(){
 
   },
   methods:{
+    myFunction(){
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
+            console.log(res.data.response);
+            this.emailRandom.push( res.data.response ) 
+          })
+        }
+    }
 
   }
 }).mount('#app')
