90c5334d807e5fa18f1e28107acda8c89641f843
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-04-25 11:54:53 +0200

    modifiche estetiche

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ const { createApp } = Vue
 createApp({
   data(){
     return {
-        emailRandom: []
+      emailRandom: []
     }
   },
   mounted(){
@@ -20,7 +20,7 @@ createApp({
   methods:{
     myFunction(){
         for (let i = 0; i < 10; i++) {
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
+          axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
             console.log(res.data.response);
             this.emailRandom.push( res.data.response ) 
           })
